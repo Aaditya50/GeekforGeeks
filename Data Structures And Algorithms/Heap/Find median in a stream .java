154b5bf671fc5f767b1e23a7c0e1c645291ebbb8
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2022-04-27 00:43:07 +0530

    Create Find median in a stream .java

---
+++ Data Structures And Algorithms/Heap/Find median in a stream .java
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+
+class GFG
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        int n;
+        while(t-- > 0){
+            n = sc.nextInt();
+    
+            Solution obj = new Solution();
+            for(int i = 1; i <= n; i++)
+            {
+                int x =sc.nextInt();
+                obj.insertHeap(x);
+                System.out.println((int)Math.floor(obj.getMedian()));
+            }
+        }
+        
+        
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{   
+    static PriorityQueue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
+    static PriorityQueue<Integer> right = new PriorityQueue<>();
+    //Function to insert heap.
+    public static void insertHeap(int x)
+    {
+            left.add(x);
+            right.add(left.remove());
+            balanceHeaps();
+    }
+    
+    public static void balanceHeaps()
+    {       
+      if(right.size()>left.size()){
+           left.add(right.peek());
+           right.poll();
+       }
+    }
+    
+    public static double getMedian()
+    {   
+        if(left.size() == right.size() ){
+            int i = left.peek();
+            int j = right.peek();
+            return (double)(i+j)/2;
+        }
+        return (double)left.peek();
+    }
+
+    
+}
