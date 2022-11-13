7cedd3548f43ce48db48f7ad712d6ae5955eba17
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2022-11-13 20:23:13 +0530

    Create Fill up buckets.java

---
+++ Data Structures And Algorithms/Math/Fill up buckets.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine().trim();
+            String[] S = s.split(" ");
+            int[] capacity = new int[n];
+            for(int i = 0; i < n; i++)
+                capacity[i] = Integer.parseInt(S[i]);
+            Solution ob = new Solution();
+            int ans = ob.totalWays(n, capacity);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int totalWays(int n, int[] capacity) {
+        // code here
+        long sum=1;
+        
+        if(n==1) return capacity[0];
+        else {
+            Arrays.sort(capacity);
+            for(int i=0;i<n;i++){
+                sum=(sum * (capacity[i]-i))%1000000007;
+            }
+        }
+        return (int)sum;
+    }
+}
